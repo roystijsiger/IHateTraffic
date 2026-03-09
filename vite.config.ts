1e3ad2919c24f4731df532d5fcb33e7a93c339d9
Roy Stijsiger <roy.stijsiger@outlook.com>
2026-03-09 11:57:49 +0100

    Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/traffic-app/', // Vervang dit met je repository naam
+  base: '/IHateTraffic/',
   plugins: [
     vue(),
     vueDevTools(),
